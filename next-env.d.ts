4b98a994f393b4ca492b00fb606f16db4c923e19
Ryu236 <ryu.adv.2360@gmail.com>
2021-02-09 21:04:36 +0900

    :sparkles: feat(googleAnalytics): add google analytics script

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,17 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+interface Window {
+  // For pageview
+  gtag(type: 'config', googleAnalyticsId: string, { page_path: string })
+  // For event
+  gtag(
+    type: 'event',
+    eventAction: string,
+    fieldObject: {
+      event_label: string
+      event_category: string
+      value?: number
+    }
+  )
+}
