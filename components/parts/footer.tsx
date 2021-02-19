1ed24bb76283ff15ad7c214f82594c322b0bca13
Ryu236 <ryu.adv.2360@gmail.com>
2021-02-19 20:37:43 +0900

    :lipstick: style(footer): add style tag

--- components/parts/footer.tsx
+++ components/parts/footer.tsx
@@ -2,14 +2,37 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer>
-      <a
-        href="https://twitter.com/Ryuk236"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        © 2021 Ryutaro Kobayashi
-      </a>
-    </footer>
+    <>
+      <footer>
+        <a
+          href="https://twitter.com/Ryuk236"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          © 2021 Ryutaro Kobayashi
+        </a>
+      </footer>
+
+      <style jsx global>{`
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid var(--border-color);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+    </>
   )
 }
