e153dd85c3ecd0e448e08b3dcf6d14f6570abbb4
Ryu236 <ryu.adv.2360@gmail.com>
2021-04-04 23:57:38 +0900

    :+1: feat(resume): add education part

--- components/parts/resume.tsx
+++ components/parts/resume.tsx
@@ -4,6 +4,18 @@ export const Resume: React.FC<{}> = () => {
   return (
     <>
       <h1>Resume</h1>
+      <h2>Education</h2>
+      <div>
+        Apr 2013 - Mar 2017 <br />
+        Shinshu University Faculty of Engineering Computer Science and
+        Engineering
+      </div>
+      <div>
+        Apr 2017 - Mar 2019 <br />
+        Shinshu University Graduate School of Science and Technology
+        (Master&#39;s Program)
+      </div>
+      <h2>Career History</h2>
     </>
   )
 }
