d999feada9e1fe5c4285f1d1e15ece91cb64dc86
Ryu236 <ryu.adv.2360@gmail.com>
2021-04-11 19:25:45 +0900

    :+1: feat(resume): add career history about internship

--- components/parts/resume.tsx
+++ components/parts/resume.tsx
@@ -6,16 +6,34 @@ export const Resume: React.FC<{}> = () => {
       <h1>Resume</h1>
       <h2>Education</h2>
       <div>
-        Apr 2013 - Mar 2017 <br />
-        Shinshu University Faculty of Engineering Computer Science and
-        Engineering
-      </div>
-      <div>
         Apr 2017 - Mar 2019 <br />
         Shinshu University Graduate School of Science and Technology
         (Master&#39;s Program)
       </div>
+      <div>
+        Apr 2013 - Mar 2017 <br />
+        Shinshu University Faculty of Engineering Computer Science and
+        Engineering
+      </div>
       <h2>Career History</h2>
+      <div>
+        Apr 2019 - Now <br />
+        DMM.com Software Engineer
+        <br />
+        Development payment platform
+      </div>
+      <div>
+        Sep 2018 - Sep 2018 <br />
+        Eureka Internship
+        <br />
+        Development Android application
+      </div>
+      <div>
+        Sep 2017 - Sep 2017 <br />
+        SONY Internship
+        <br />
+        Development embedded system
+      </div>
     </>
   )
 }
